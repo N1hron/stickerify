e4a0c4bbecb9f481de0015f7e1857e9a58f2b2da
N1hron <n1hron@yandex.com>
2025-03-06 02:09:40 +0300

    Add link to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,15 @@
-import { Card } from '../ui';
+import { Card, Link } from '../ui';
 
 import styles from './style.module.scss';
 
 function Footer() {
-    return <Card className={styles.footer} as='footer'></Card>;
+    return (
+        <Card className={styles.footer} as='footer'>
+            <Link href='https://github.com/N1hron/stickerify' aria-label='GitHub'>
+                GitHub page
+            </Link>
+        </Card>
+    );
 }
 
 export { Footer };
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,3 +4,4 @@ export { Label } from './label/Label';
 export { Button } from './button/Button';
 export { Checkbox } from './checkbox/Checkbox';
 export { SpinButton } from './spin-button/SpinButton';
+export { Link } from './link/Link';
