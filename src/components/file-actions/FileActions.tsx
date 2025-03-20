06912f0351be7fd34d048ca0fce92d6dbb612dd0
N1hron <n1hron@yandex.com>
2025-03-21 00:01:59 +0300

    Add remove files button

--- src/components/file-actions/FileActions.tsx
+++ src/components/file-actions/FileActions.tsx
@@ -1,8 +1,8 @@
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { selectSetting } from '../../store/slices/settings';
-import { addFiles } from '../../store/slices/files';
+import { addFiles, removeSelectedFiles, selectAllowRemove } from '../../store/slices/files';
 import { config } from '../../config';
-import { Card, FileInput } from '../ui';
+import { Card, FileInput, Button } from '../ui';
 
 import styles from './style.module.scss';
 
@@ -11,14 +11,23 @@ function FileActions() {
 
     const stickerMotion = useAppSelector(selectSetting('stickerMotion'));
     const accept = config.acceptValues[stickerMotion];
+    const allowRemove = useAppSelector(selectAllowRemove);
 
     function handleFilesChange(files: File[]) {
         dispatch(addFiles(files));
     }
 
+    function handleRemoveClick() {
+        if (!allowRemove) return;
+        dispatch(removeSelectedFiles());
+    }
+
     return (
         <Card className={styles.fileActions} mini>
             <FileInput label='Add files' mini accept={accept} onChange={handleFilesChange} />
+            <Button color='danger' mini onClick={handleRemoveClick} disabled={!allowRemove}>
+                Remove
+            </Button>
         </Card>
     );
 }
