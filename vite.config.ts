83ca060ec3b894bf2dc19532f83960d78fd22480
N1hron <n1hron@yandex.com>
2025-03-21 22:35:47 +0300

    Add ffmpeg

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+  },
   server: {
     host: true,
     port: 8080,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
   },
 });
