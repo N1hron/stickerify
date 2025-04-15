04ac8559b2592c3e1fca1edc3875b4ca9cd9da75
N1hron <n1hron@yandex.com>
2025-04-15 19:20:48 +0300

    Temporarily remove file count

--- src/components/transcoder/actions/Add.tsx
+++ src/components/transcoder/actions/Add.tsx
@@ -1,33 +1,24 @@
 import { useAppDispatch, useAppSelector } from '@store/hooks';
-import { addFiles, selectAllowAdd, selectFilesAmount } from '@slices/transcoder';
-import { FILE_LIMIT, FILE_ACCEPT } from '@/config';
-import { FileInput, Divider } from '@ui';
-
-import styles from './style.module.scss';
+import { addFiles, selectAllowAdd } from '@slices/transcoder';
+import { FILE_ACCEPT } from '@/config';
+import { FileInput } from '@ui';
 
 function Add() {
     const dispatch = useAppDispatch();
     const allowAdd = useAppSelector(selectAllowAdd);
-    const filesAmount = useAppSelector(selectFilesAmount);
 
     function handleFilesChange(files: File[]) {
         dispatch(addFiles(files));
     }
 
     return (
-        <div className={styles.add}>
-            <FileInput
-                label='Add'
-                accept={FILE_ACCEPT}
-                disabled={!allowAdd}
-                mini
-                onChange={handleFilesChange}
-            />
-            <div className={styles.fileCount}>
-                {filesAmount} / {FILE_LIMIT}
-            </div>
-            <Divider vertical />
-        </div>
+        <FileInput
+            label='Add'
+            accept={FILE_ACCEPT}
+            disabled={!allowAdd}
+            mini
+            onChange={handleFilesChange}
+        />
     );
 }
 
