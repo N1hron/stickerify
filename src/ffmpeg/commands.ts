1f210c80e65ace7808224564ec1cb1c63f548ed4
Denis <n1hron@yandex.com>
2025-10-20 17:29:48 +0300

    Add JSDoc comments for ffmpeg commands

--- src/ffmpeg/commands.ts
+++ src/ffmpeg/commands.ts
@@ -1,28 +1,28 @@
-// Extract only first frame
+/** Extract only first frame */
 const oneFrame = ['-frames:v', '1'];
 
-// Set max quality for webp
+/** Set max quality for webp */
 const maxQualityWebp = ['-quality', '100'];
 
-// Scale up and down, remove empty spaces
+/** Scale up and down, remove empty spaces */
 const scaleTrimSpaces = (sizePx: number) => [
     '-vf',
     `scale=${sizePx}:${sizePx}:force_original_aspect_ratio=decrease`,
 ];
 
-// Scale up and down, keep empty spaces
+/** Scale up and down, keep empty spaces */
 const scaleKeepSpaces = (sizePx: number, padX: string, padY: string) => [
     '-vf',
     `scale=${sizePx}:${sizePx}:force_original_aspect_ratio=decrease, pad=${sizePx}:${sizePx}:${padX}:${padY}:color=0x00000000`,
 ];
 
-// Scale down if source too big, remove empty spaces where possible
+/** Scale down if source is too big, remove empty spaces if possible */
 const scaleDownTrimSpaces = (sizePx: number, padX: string) => [
     '-vf',
     `scale='min(iw, ${sizePx})':'min(ih, ${sizePx})':force_original_aspect_ratio=decrease, pad='if(gte(ih, ${sizePx}), min(iw, ${sizePx}), ${sizePx})':'min(ih, ${sizePx})':${padX}:0:color=0x00000000`,
 ];
 
-// Scale down if source too big, keep empty spaces
+/** Scale down if source is too big, keep empty spaces */
 const scaleDownKeepSpaces = (sizePx: number, padX: string, padY: string) => {
     return [
         '-vf',
